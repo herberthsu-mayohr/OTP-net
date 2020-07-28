94b5322460fcac709199b99bc9ee69c98644e61b
herbert_hsu@mayohr <herbert_hsu@mayohr.com>
2020-07-28 17:10:39 +0800

    Profile/Token lab exercise

--- RsaSecureTokenTests/AuthenticationServiceTests.cs
+++ RsaSecureTokenTests/AuthenticationServiceTests.cs
@@ -6,15 +6,26 @@ namespace RsaSecureToken.Tests
     [TestFixture]
     public class AuthenticationServiceTests
     {
-        [Test()]
+        [Test]
         public void is_valid()
         {
-            var target = new AuthenticationService();
+            // var target = new AuthenticationService();
+            var target = new AuthenticationService(new FakeProfile(), new FakeToken());
 
             var actual = target.IsValid("joey", "91000000");
 
             //always failed
             Assert.IsTrue(actual);
         }
+    }
+    
+    internal class FakeProfile : IProfile
+    {
+        public string GetPassword(string account) => account == "joey" ? "91" : "";
+    }
+
+    internal class FakeToken : IToken
+    {
+        public string GetRandom(string account) => "000000";
     }
 }
