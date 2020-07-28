221a67ac313dd6dd87b51f48f569a01fd49b2758
herbert_hsu@mayohr <herbert_hsu@mayohr.com>
2020-07-28 17:49:37 +0800

    Profile/Token stub + refactor lab exercise

--- RsaSecureTokenTests/AuthenticationServiceTests.cs
+++ RsaSecureTokenTests/AuthenticationServiceTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
 using Assert = NUnit.Framework.Assert;
 
 namespace RsaSecureToken.Tests
@@ -6,16 +8,36 @@ namespace RsaSecureToken.Tests
     [TestFixture]
     public class AuthenticationServiceTests
     {
+        private readonly IProfile _fakeProfile = Substitute.For<IProfile>();
+        private readonly IToken _fakeToken = Substitute.For<IToken>();
+        private readonly AuthenticationService _target;
+
+        public AuthenticationServiceTests()
+        {
+            _target = new AuthenticationService(_fakeProfile, _fakeToken);
+        }
+
         [Test]
         public void is_valid()
         {
-            // var target = new AuthenticationService();
-            var target = new AuthenticationService(new FakeProfile(), new FakeToken());
+            GivenProfile("joey", "91");
+            GivenToken("000000");
+            ShouldBeValid("joey", "91000000");
+        }
 
-            var actual = target.IsValid("joey", "91000000");
+        private void ShouldBeValid(string account, string password)
+        {
+            _target.IsValid(account, password).Should().BeTrue();
+        }
 
-            //always failed
-            Assert.IsTrue(actual);
+        private void GivenToken(string token)
+        {
+            _fakeToken.GetRandom("").ReturnsForAnyArgs(token);
+        }
+
+        private void GivenProfile(string account, string password)
+        {
+            _fakeProfile.GetPassword(account).Returns(password);
         }
     }
     
