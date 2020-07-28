2dfb2aaa622749c22e46557355151d6b3c0e967f
herbert_hsu@mayohr <herbert_hsu@mayohr.com>
2020-07-28 15:43:59 +0800

    Holiday lab exercise

---
+++ RsaSecureToken/Holiday.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RsaSecureToken
+{
+    public class Holiday
+    {
+        public string SayHello()
+        {
+            var today = GetToday();
+            return today.Month == 12 || today.Day == 25 ? "Xmas" : "Today is not Xmas";
+        }
+
+        protected virtual DateTime GetToday()
+        {
+            return DateTime.Now;
+        }
+    }
+}
---
+++ RsaSecureTokenTests/HolidayTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace RsaSecureToken.Tests
+{
+    [TestFixture]
+    public class HolidayTests
+    {
+        private HolidayForTest _holidayForTest;
+
+        [SetUp]
+        public void Setup()
+        {
+            _holidayForTest = new HolidayForTest();
+        }
+        
+        [Test]
+        public void Today_Is_Xmas()
+        {
+            GivenToday(2020, 12, 25);
+            ResponseShouldBe("Xmas");
+        }
+
+        [Test]
+        public void Today_Is_Not_Xmas()
+        {
+            GivenToday(2020, 7, 28);
+            ResponseShouldBe("Today is not Xmas");
+        }
+
+        private void ResponseShouldBe(string expectedResult)
+        {
+            var actualResult = _holidayForTest.SayHello();
+            actualResult.Should().BeEquivalentTo(expectedResult);
+        }
+
+        private void GivenToday(int year, int month, int day)
+        {
+            _holidayForTest.Today = new DateTime(year, month, day);
+        }
+
+        private class HolidayForTest : Holiday
+        {
+            private DateTime _today;
+
+            protected override DateTime GetToday()
+            {
+                return _today;
+            }
+
+            public DateTime Today
+            {
+                set => _today = value;
+            }
+        }
+    }
+}
